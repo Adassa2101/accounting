c2fd4a3cb4b180ce61b05e0d98c43546a8a47680
Adassa2101 <90108698+Adassa2101@users.noreply.github.com>
2025-08-26 22:39:48 +0300

    hw endpoint/account/login

--- src/features/api/accountAPI.ts
+++ src/features/api/accountAPI.ts
@@ -23,4 +23,26 @@ export const registerUser = createAsyncThunk(
         const token = createToken(user.login, user.password);
         return {user:data, token};
     }
+)//todo hw endpoint/account/login
+export const fetchUser = createAsyncThunk(
+    'user/fetch',
+    async (token:string) => {
+        const response = await fetch(`${base_url}/account/fetch`, {
+            method: "POST",
+            headers: {
+                Authorization:token,
+            },
+
+        });
+        if (response.status === 401) {
+            throw new Error(`Invalid credentials`);
+        }
+        if (!response.ok){
+            throw new Error(`Something went wrong`);
+        }
+        const data = await response.json();
+        return {user:data, token};
+
+    }
+
 )
