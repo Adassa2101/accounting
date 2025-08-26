2787a6d0992023a392ab4915730444a8c5c3061a
Adassa2101 <90108698+Adassa2101@users.noreply.github.com>
2025-08-26 22:12:42 +0300

    hw endpoint account login

---
+++ src/features/api/accountAPI.ts
@@ -0,0 +1,48 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import type { UserRegister} from "../../utils/types";
+import {base_url, createToken} from "../../utils/constants.ts";
+
+export const registerUser = createAsyncThunk(
+    'user/registerUser',
+    async (user:UserRegister) => {
+        const response = await fetch(`${base_url}/account/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+
+        });
+        if (response.status === 409) {
+            throw new Error(`User ${user.login} already exists`);
+        }
+        if (!response.ok) {
+            throw new Error(`Something went wrong`);
+        }
+        const data = await response.json();
+        const token = createToken(user.login, user.password);
+        return {user:data, token};
+    }
+)//todo hw endpoint/account/login
+export const fetchUser = createAsyncThunk(
+    'user/fetch',
+    async (token:string) => {
+        const response = await fetch(`${base_url}/account/fetch`, {
+            method: "POST",
+            headers: {
+                Authorization:token,
+            },
+
+        });
+        if (response.status === 401) {
+            throw new Error(`Invalid credentials`);
+        }
+        if (!response.ok){
+            throw new Error(`Something went wrong`);
+        }
+        const data = await response.json();
+        return {user:data, token};
+
+    }
+
+)
